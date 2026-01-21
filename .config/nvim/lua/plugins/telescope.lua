c17dbbd509c30a3f5c00412351d87075a7f5c87b
Phil Standen <philip.ric.standen@hpe.com>
2026-01-21 22:28:26 +0000

    latest nvim files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,21 @@
 return {
-	'nvim-telescope/telescope.nvim', 
-	tag = '0.1.8', 
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function () 
+	config = function()
+		local tscope = require('telescope')
+		tscope.setup {
+			defaults = {
+				path_display = { "smart" }
+			},
+			pickers = {
+				buffers = {
+					sort_lastused = true,
+					theme = "dropdown",
+					path_display = { "smart" },
+				},
+			}
+		}
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
